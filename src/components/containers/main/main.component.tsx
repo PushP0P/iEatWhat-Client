30f8c7b60f186ea23fdc224549c56e9aa67d0b70
Raven <rdev1163@outlook.com>
2018-02-02 17:19:27 -0800

    refactored rest.service

--- src/components/containers/main/main.component.tsx
+++ src/components/containers/main/main.component.tsx
@@ -1,17 +1,11 @@
 import * as React from 'react';
 import { MAIN_ROUTES_SWITCH } from  '../../../router/main.routes';
 import { MainComponentProps, MainComponentState } from '../../../models/main.model';
-import { httpRequest } from '../../../services/rest-service';
 
 export class MainComponent extends React.Component<MainComponentProps, MainComponentState> {
 
 	constructor(public props: MainComponentProps) {
 		super(props);
-	}
-
-	public async componentDidMount(): Promise<void> {
-		const response: {} = await httpRequest('//localhost:5000/json');
-		console.log('Response', response );
 	}
 
 	public render() {
