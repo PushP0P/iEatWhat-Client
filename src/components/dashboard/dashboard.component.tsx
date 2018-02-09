547991c98853650c6ed069de525b00059d3e409d
Patrick Lippert <patrick.lippert1@gmail.com>
2018-02-08 20:20:46 -0800

    Added mixin for button overwrite, added buttons for dashboard.

---
+++ src/components/dashboard/dashboard.component.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import {
+	DASHBOARD_COMPONENT_STATE_INIT, DashboardComponentProps,
+	DashboardComponentState
+} from '../../models/dashboard.model';
+
+export class DashboardComponent extends React.Component<DashboardComponentProps, DashboardComponentState> {
+	constructor(public props: DashboardComponentProps) {
+		super(props);
+		this.state = DASHBOARD_COMPONENT_STATE_INIT;
+	}
+
+	public render() {
+		return (
+			<div>
+				<div
+					className="container-fluid"
+				>
+					<div
+						className="row"
+					>
+						<div
+							className="col-lg-3 col-md-6 col-sm-6"
+						>
+							<button
+								className="btn btn-primary btn-dashboard"
+							>
+								Loren Ipsum
+							</button>
+						</div>
+
+						<div
+							className="col-lg-3 col-md-6 col-sm-6"
+						>
+							<button
+								className="btn-dashboard"
+							>
+								Loren Ipsum
+							</button>
+						</div>
+
+						<div
+							className="col-lg-3 col-md-6 col-sm-6"
+						>
+							<button
+								className="btn-dashboard"
+							>
+								Loren Ipsum
+							</button>
+						</div>
+
+						<div
+							className="col-lg-3 col-md-6 col-sm-6"
+						>
+							<button
+								className="btn-dashboard"
+							>
+								Loren Ipsum
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
